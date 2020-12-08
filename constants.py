cde7374d147f2d9e7bc43765e611955ca90604c2
Andrew Apodaca <aapodaca13@live.com>
2020-12-08 02:32:57 -0800

    Updates Nike class to use real time information

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 class Constants:
     # TODO convert this into linux file path later
     CONFIG_FILE_PATH = "config/config.json"
+    PATH_TO_NIKE_ACTIVITIES = '/api/running_page-master/activities'
