7cae0640023bf7724ef8eee18feffc1a3b16eecb
Andrew Apodaca <aapodaca13@live.com>
2020-12-09 18:15:12 -0800

    Updated to reflect current data and working totals

--- api_nike.py
+++ api_nike.py
@@ -3,43 +3,85 @@ import json
 from constants import Constants
 
 test = None
-input_miles = 0
-input_cals = 0
 
+
+# See nike_sync.py for file pull script
 # Navigates through JSON files that have been pulled and returns miles and cals from most recent run. Rounds float to 2
-for x, y, files in os.walk(os.path.abspath(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES)):
-    test = open(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES + '\\' + files[0], "r")
-    test2 = json.load(test)
-    for i in test2['summaries']:
-        if i['metric'] == 'distance':
-            input_miles = i['value']
-            input_miles = round(input_miles, 2)
-        if i['metric'] == 'calories':
-            input_cals = i['value']
-            input_cals = round(input_cals, 2)
 
 
 class Nike:
+    input_miles = 0
+    input_cals = 0
+    total_input_miles = 0
+    total_input_cals = 0
 
     def __init__(self, miles, cals):
         self.miles = miles
         self.cals = cals
 
+# Navigates through JSON files that have been pulled and returns miles and cals from most recent run. Rounds float to 2
+    @staticmethod
+    def GetLatests():
+        for x, y, files in os.walk(os.path.abspath(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES)):
+            sorted_files = sorted(files)
+            test = open(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES + '\\' + sorted_files[-1], "r")
+            test2 = json.load(test)
+            for i in test2['summaries']:
+                if i['metric'] == 'distance':
+                    Nike.input_miles = i['value']
+                    Nike.input_miles = round(Nike.input_miles, 2)
+
+                if i['metric'] == 'calories':
+                    Nike.input_cals = i['value']
+                    Nike.input_cals = round(Nike.input_cals, 2)
+    @staticmethod
+    def GetTotals():
+        for x, y, files in os.walk(os.path.abspath(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES)):
+            sorted_files = sorted(files)
+            for curr_file in sorted_files:
+                test = open(os.curdir + Constants.PATH_TO_NIKE_ACTIVITIES + '\\' + curr_file, "r")
+                test2 = json.load(test)
+                for i in test2['summaries']:
+                    if i['metric'] == 'distance':
+                        temp_total = i['value']
+                        Nike.total_input_miles += temp_total
+
+                    if i['metric'] == 'calories':
+                        temp_total = i['value']
+                        Nike.total_input_cals += temp_total
+
+                Nike.total_input_miles = round(Nike.total_input_miles, 2)
+                Nike.total_input_cals = round(Nike.total_input_cals, 2)
+
+
+
 # Call these 4 functions for latest or total miles or calories. Totals functions are incomplete
-    def LatestMiles(self):
-        print(self.miles)
-        return self.miles
+    @staticmethod
+    def LatestMiles():
+        Nike.GetLatests()
+        print(Nike.input_miles)
+        return Nike.input_miles
 
-    def LatestCals(self):
-        print(self.cals)
-        return self.cals
+    @staticmethod
+    def LatestCals():
+        Nike.GetLatests()
+        print(Nike.input_cals)
+        return Nike.input_cals
 
-# Still working on these
-    def TotalMiles(self):
-        return self.miles*2
+    @staticmethod
+    def TotalMiles():
+        Nike.GetTotals()
+        print(Nike.total_input_miles)
+        return Nike.total_input_miles
 
-    def TotalCals(self):
-        return self.cals*2
+    @staticmethod
+    def TotalCals():
+        Nike.GetTotals()
+        print(Nike.total_input_cals)
+        return Nike.total_input_cals
 
 
-nike = Nike(input_miles, input_cals)
+Nike.LatestMiles()
+Nike.LatestCals()
+Nike.TotalMiles()
+Nike.TotalCals()
