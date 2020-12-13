9a4ca4ec551b349ddaaf7b6facc48e565557099e
Joshua Luceno <joshualuceno@gmail.com>
2020-12-13 02:31:12 -0800

    Integrated everything

--- config_main.py
+++ config_main.py
@@ -83,6 +83,3 @@ def window():
 
     win.show()
     sys.exit(app.exec())
-
-
-window()
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import logging
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from config_main import MainWindow
 from task_manager import TaskManager
 
 
 def main():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     logging.debug("Starting project focus")
-    TaskManager.start()
 
+    # Setup the UI
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+
+    # Start the task manager thread
+    tm = TaskManager()
+    TaskManager.config_class = win
+    tm.start()
+
+    # Start the Qt application
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
+
