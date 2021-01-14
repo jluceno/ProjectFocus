53117e41081e9650451c31e05f367ca84764f934
Joshua Luceno <joshualuceno@gmail.com>
2021-01-13 19:25:07 -0800

    Updated constants file

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
+from pathlib import Path
+
 class Constants:
-    # TODO convert this into linux file path later
-    CONFIG_FILE_PATH = "config/config.json"
-    PATH_TO_NIKE_ACTIVITIES = 'api/running_page/activities'
+    CONFIG_FILE_PATH = Path(".", "config", "config.json")
+    PATH_TO_NIKE_ACTIVITIES =  Path(".", "api", "running_page", "activities")
