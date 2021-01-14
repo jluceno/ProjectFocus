ac59426988b5f895a43999ae3803e21a934ca78f
Joshua Luceno <joshualuceno@gmail.com>
2021-01-13 21:31:59 -0800

    Made paths system agnostic

--- api_nike.py
+++ api_nike.py
@@ -2,6 +2,7 @@ import os
 import json
 import threading
 import time
+from pathlib import Path
 from constants import Constants
 from api.running_page.scripts.nike_sync import run as sync_token_run
 
@@ -25,54 +26,65 @@ class Nike():
             Nike.JSON_pull()
             time.sleep(1800)
 
-# Populates 'activities' folder with JSON files. See https://github.com/yihong0618/running_page#nike-run-club for Key.
+    # Populates 'activities' folder with JSON files. See https://github.com/yihong0618/running_page#nike-run-club for Key.
     @staticmethod
     def JSON_pull():
         Nike.lock.acquire()
         cwd = os.getcwd()
-        os.chdir(".\\api\\running_page")
+        os.chdir(Path('.', 'api', 'running_page').resolve())
         sync_token_run(Nike.auth_key)
         os.chdir(cwd)
         Nike.lock.release()
 
-# Navigates through JSON files that have been pulled and returns miles and cals from most recent run. Rounds float to 2
+    # Navigates through JSON files that have been pulled and returns miles and cals from most recent run. Rounds float to 2
     @staticmethod
     def GetLatests():
         Nike.lock.acquire()
-        for x, y, files in os.walk(os.path.abspath(os.curdir + '/' + Constants.PATH_TO_NIKE_ACTIVITIES)):
-            sorted_files = sorted(files)
-            test = open(os.curdir + '/' + Constants.PATH_TO_NIKE_ACTIVITIES + '/' + sorted_files[-1], "r")
-            test2 = json.load(test)
-            for i in test2['summaries']:
-                if i['metric'] == 'distance':
-                    Nike.input_miles = i['value']
-                    Nike.input_miles = round(Nike.input_miles, 2)
+        
+        # Find the latest file
+        files = []
+        for file in Constants.PATH_TO_NIKE_ACTIVITIES.iterdir():
+            files.append(file)
 
-                if i['metric'] == 'calories':
-                    Nike.input_cals = i['value']
-                    Nike.input_cals = round(Nike.input_cals, 2)
+        sorted_files = sorted(files)
+        test = Path(sorted_files[-1]).open(mode = 'r')
+        test2 = json.load(test)
+        for i in test2['summaries']:
+            if i['metric'] == 'distance':
+                Nike.input_miles = i['value']
+                Nike.input_miles = round(Nike.input_miles, 2)
+
+            if i['metric'] == 'calories':
+                Nike.input_cals = i['value']
+                Nike.input_cals = round(Nike.input_cals, 2)
         Nike.lock.release()
 
-# Finds and returns totals for all JSON files combined.
+    # Finds and returns totals for all JSON files combined.
     @staticmethod
     def GetTotals():
         Nike.lock.acquire()
         Nike.total_input_cals = 0
         Nike.total_input_miles = 0
-        for x, y, files in os.walk(os.path.abspath(os.curdir + '/' + Constants.PATH_TO_NIKE_ACTIVITIES)):
-            sorted_files = sorted(files)
-            for curr_file in sorted_files:
-                test = open(os.curdir + '/' + Constants.PATH_TO_NIKE_ACTIVITIES + '/' + curr_file, "r")
-                test2 = json.load(test)
-                for i in test2['summaries']:
-                    if i['metric'] == 'distance':
-                        temp_total = i['value']
-                        Nike.total_input_miles += temp_total
-                    if i['metric'] == 'calories':
-                        temp_total = i['value']
-                        Nike.total_input_cals += temp_total
-                Nike.total_input_miles = round(Nike.total_input_miles, 2)
-                Nike.total_input_cals = round(Nike.total_input_cals, 2)
+
+        # Read all files
+        files = []
+        for file in Constants.PATH_TO_NIKE_ACTIVITIES.iterdir():
+            files.append(file)
+
+        # Sort files and sum everything up
+        sorted_files = sorted(files)
+        for curr_file in sorted_files:
+            test = Path(curr_file).open(mode = 'r')
+            test2 = json.load(test)
+            for i in test2['summaries']:
+                if i['metric'] == 'distance':
+                    temp_total = i['value']
+                    Nike.total_input_miles += temp_total
+                if i['metric'] == 'calories':
+                    temp_total = i['value']
+                    Nike.total_input_cals += temp_total
+            Nike.total_input_miles = round(Nike.total_input_miles, 2)
+            Nike.total_input_cals = round(Nike.total_input_cals, 2)
         Nike.lock.release()
 
 # Call these 4 functions for latest or total miles or calories. Totals functions are incomplete
